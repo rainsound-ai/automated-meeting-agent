b9ee677e95413082aa334c4eb803609d677e7dae
iandoesallthethings <iandoesallthethings@gmail.com>
2024-11-05 12:21:47 -0500

    Set up cron job.

--- backend/app/lib/Env.py
+++ backend/app/lib/Env.py
@@ -3,8 +3,20 @@ import os
 
 load_dotenv()
 
+test = os.getenv("USE_TEST_DB") == "true"
+
+
+def test_or_prod(variable: str) -> str:
+    prefix = "TEST_" if test else ""
+    return os.getenv(f"{prefix}{variable}")
+
+
 environment = os.getenv("ENVIRONMENT")
 open_ai_api_key = os.getenv("OPENAI_API_KEY")
 notion_api_key = os.getenv("NOTION_API_KEY")
-rainsound_link_summary_database_id= os.getenv("RAINSOUND_LINK_SUMMARY_DATABASE_ID")
-rainsound_meeting_summary_database_id = os.getenv("RAINSOUND_MEETING_SUMMARY_DATABASE_ID")
+
+
+rainsound_link_summary_database_id = test_or_prod("RAINSOUND_LINK_SUMMARY_DATABASE_ID")
+rainsound_meeting_summary_database_id = test_or_prod(
+    "RAINSOUND_MEETING_SUMMARY_DATABASE_ID"
+)
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,32 @@
-from app.lib.Env import environment
-import logging
 import asyncio
-from app.api.update_notion_with_transcript_and_summary import update_notion_with_transcript_and_summary
+import logging
+import time
+
+import schedule
+from app.api.update_notion_with_transcript_and_summary import (
+    update_notion_with_transcript_and_summary,
+)
+from app.lib.Env import environment
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.info(f"💡 Running in {environment} environment")
+
+
+def main():
+    print("🌺 Running initial task")
+    asyncio.run(run_update_task())
+
+    print("Scheduling tasks")
+    schedule.every(15).minutes.do(run_update_task)
+
+    while True:
+        print("🌺 Running scheduled tasks")
+        schedule.run_pending()
+        time.sleep(1)
+
 
 async def run_update_task():
     try:
@@ -17,5 +36,7 @@ async def run_update_task():
     except Exception as e:
         logger.error(f"🚨 Error in Notion update task: {str(e)}")
 
+
 if __name__ == "__main__":
-    asyncio.run(run_update_task())
+    main()
+    # asyncio.run(run_update_task())
